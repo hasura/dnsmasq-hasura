3f00bf1369e7bca084cad0c7ac6443a56a85b499
Tanmai Gopal <tanmaig@34cross.in>
2016-10-18 18:55:36 +0530

    Adds setup file for DNSMasq working on Hasura on mac

---
+++ mac/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install dnsmasq
+brew install dnsmasq
+
+# Load dnsmasq on start
+sudo cp $(brew list dnsmasq | grep /homebrew.mxcl.dnsmasq.plist$) /Library/LaunchDaemons/
+sudo launchctl load /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist
+dscacheutil -flushcache
+
+# Add a resolver
+sudo mkdir -p /etc/resolver
+sudo tee /etc/resolver/.hasura.test > /dev/null <<EOF
+nameserver 127.0.0.1
+EOF
+
+# use minikube ip in dnsmasq.conf
+minikube start
+echo "address=/hasura.test/$(minikube ip)" > dnsmasq.conf
+sudo mv dnsmasq.conf /usr/local/etc/dnsmasq.conf
+
+# restart dnsmasq
+sudo launchctl stop homebrew.mxcl.dnsmasq
---
+++ mac/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+tee .hasura.test > /dev/null <<EOF
+nameserver 127.0.0.1
+EOF
